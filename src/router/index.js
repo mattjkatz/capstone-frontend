52bacde9f754fd1dd17a2fc8b5c5fe35d70774ac
mattjkatz <matthewkatz7@gmail.com>
2022-04-28 20:37:54 -0500

    update the empty frontend to include axios and set up the empty home vue file

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import axios from "axios";
+
+axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
 
 const routes = [
   {
