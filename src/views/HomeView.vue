03be1722b778db057fe91fc81cca497ee3a3fe47
mattjkatz <matthewkatz7@gmail.com>
2022-05-03 10:12:53 -0500

    finish the budgetIndex method and add html

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,34 @@
 <script>
+import axios from "axios";
+
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome to Matt's Budgeting App!",
+      budgets: [],
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.budgetIndex();
+  },
+  methods: {
+    budgetIndex() {
+      axios.get("/budgets").then((response) => {
+        console.log(response.data);
+        this.budgets = response.data;
+        console.log(this.budgets);
+      });
+    },
+  },
 };
 </script>
 
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+  </div>
+  <div v-for="budget in budgets" v-bind:key="budget.id">
+    <h3>{{ budget.name }}</h3>
   </div>
 </template>
 
