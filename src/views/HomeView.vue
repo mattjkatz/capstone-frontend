7d3414e6eb6942c427256e79ff4e02b4a8b3aa65
mattjkatz <matthewkatz7@gmail.com>
2022-05-03 12:17:01 -0500

    update the index HTML to display more relevant information

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,6 +6,7 @@ export default {
     return {
       message: "Welcome to Matt's Budgeting App!",
       budgets: [],
+      finances: [],
     };
   },
   created: function () {
@@ -16,7 +17,6 @@ export default {
       axios.get("/budgets").then((response) => {
         console.log(response.data);
         this.budgets = response.data;
-        console.log(this.budgets);
       });
     },
   },
@@ -28,7 +28,11 @@ export default {
     <h1>{{ message }}</h1>
   </div>
   <div v-for="budget in budgets" v-bind:key="budget.id">
-    <h3>{{ budget.name }}</h3>
+    <h2>{{ budget.name }}</h2>
+    <div v-for="finance in budget.finances" v-bind:key="finance.id">
+      <h4>{{ finance.name }}</h4>
+      <p>{{ finance.amount }} | {{ finance.category }} | {{ finance.frequency }}</p>
+    </div>
   </div>
 </template>
 
