bf7da47353e9ac75bd2f874cb4799fffc9c55a64
mattjkatz <matthewkatz7@gmail.com>
2022-05-05 20:26:18 -0500

    added a custom select input chooser

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -96,6 +96,17 @@ export default {
         Category:
         <input type="text" v-model="newPurchase.category" />
       </p>
+      <div class="input-group mb-3">
+        <div class="input-group-prepend">
+          <label class="input-group-text" for="inputGroupSelect01">Categories</label>
+        </div>
+        <select class="custom-select" id="inputGroupSelect01">
+          <option selected>Choose...</option>
+          <option value="1">One</option>
+          <option value="2">Two</option>
+          <option value="3">Three</option>
+        </select>
+      </div>
       <p>
         Frequency:
         <input type="text" v-model="newPurchase.frequency" />
