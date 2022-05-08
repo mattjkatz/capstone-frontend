60374226bdcda4d672c2bf6a84450b9eb29f0e5c
mattjkatz <matthewkatz7@gmail.com>
2022-05-08 10:02:15 -0500

    get router view to fit in on screen swith the sidebar, add a signup page

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
+import SignupView from "../views/SignupView.vue";
 import axios from "axios";
 
 axios.defaults.baseURL = process.env.NODE_ENV === "development" ? "http://localhost:3000" : "/";
@@ -11,12 +12,9 @@ const routes = [
     component: HomeView,
   },
   {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+    path: "/signup",
+    name: "signup",
+    component: SignupView,
   },
 ];
 
---
+++ src/views/SignupView.vue
@@ -0,0 +1,138 @@
+<script>
+import axios from "axios";
+export default {
+  data: function () {
+    return {
+      newUserParams: {},
+      errors: [],
+    };
+  },
+  methods: {
+    submit: function () {
+      axios
+        .post("/users", this.newUserParams)
+        .then((response) => {
+          console.log(response.data);
+          this.$router.push("/");
+        })
+        .catch((error) => {
+          this.errors = error.response.data.errors;
+        });
+    },
+  },
+};
+</script>
+
+<template>
+  <!-- <div class="signup">
+    <form v-on:submit.prevent="submit()">
+      <h1>Signup</h1>
+      <ul>
+        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+      </ul>
+      <div>
+        <label>Name:</label>
+        <input type="text" v-model="newUserParams.name" />
+      </div>
+      <div>
+        <label>Email:</label>
+        <input type="email" v-model="newUserParams.email" />
+      </div>
+      <div>
+        <label>Password:</label>
+        <input type="password" v-model="newUserParams.password" />
+        <div>
+          <small v-if="newUserParams?.password?.length > 0 && newUserParams?.password?.length < 6" class="text-danger">
+            *Password must be at least 6 characters.
+          </small>
+        </div>
+      </div>
+      <div>
+        <label>Password confirmation:</label>
+        <input type="password" v-model="newUserParams.password_confirmation" />
+        <div>
+          <small v-if="newUserParams.password_confirmation !== newUserParams.password" class="text-danger">
+            *Confirmation must match your password.
+          </small>
+        </div>
+      </div>
+      <input type="submit" value="Submit" />
+    </form>
+  </div> -->
+  <div class="container">
+    <div class="card o-hidden border-0 shadow-lg my-5">
+      <div class="card-body p-0">
+        <!-- Nested Row within Card Body -->
+        <div class="row">
+          <div class="col-lg-5 d-none d-lg-block bg-register-image"></div>
+          <div class="col-lg-7">
+            <div class="p-5">
+              <div class="text-center">
+                <h1 class="h4 text-gray-900 mb-4">Create an Account!</h1>
+              </div>
+              <form class="user">
+                <div class="form-group row">
+                  <div class="col-sm-6 mb-3 mb-sm-0">
+                    <input
+                      type="text"
+                      class="form-control form-control-user"
+                      id="exampleFirstName"
+                      placeholder="First Name"
+                      v-model="newUserParams.first_name"
+                    />
+                  </div>
+                  <div class="col-sm-6">
+                    <input
+                      type="text"
+                      class="form-control form-control-user"
+                      id="exampleLastName"
+                      placeholder="Last Name"
+                      v-model="newUserParams.last_name"
+                    />
+                  </div>
+                </div>
+                <div class="form-group">
+                  <input
+                    type="email"
+                    class="form-control form-control-user"
+                    id="exampleInputEmail"
+                    placeholder="Email Address"
+                    v-model="newUserParams.email"
+                  />
+                </div>
+                <div class="form-group row">
+                  <div class="col-sm-6 mb-3 mb-sm-0">
+                    <input
+                      type="password"
+                      class="form-control form-control-user"
+                      id="exampleInputPassword"
+                      placeholder="Password"
+                      v-model="newUserParams.password"
+                    />
+                  </div>
+                  <div class="col-sm-6">
+                    <input
+                      type="password"
+                      class="form-control form-control-user"
+                      id="exampleRepeatPassword"
+                      placeholder="Confirm Password"
+                      v-model="newUserParams.password_confirmation"
+                    />
+                  </div>
+                </div>
+                <a href="/" class="btn btn-primary btn-user btn-block">Register Account</a>
+              </form>
+              <hr />
+              <div class="text-center">
+                <a class="small" href="forgot-password.html">Forgot Password?</a>
+              </div>
+              <div class="text-center">
+                <a class="small" href="login.html">Already have an account? Login!</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
