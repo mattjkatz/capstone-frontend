ed7170d7ecc617e757302efa86f0ef0c06f7cc82
mattjkatz <matthewkatz7@gmail.com>
2022-05-10 20:13:26 -0500

    start on making a card and a list to show all of the users purchases

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -167,7 +167,55 @@ export default {
     </div>
   </div>
 
-  <!-- Earnings (Monthly) Card Example -->
+  <!-- New Content Row -->
+
+  <div class="row">
+    <!-- Purchase Index Card -->
+    <div class="col-xl-8 col-lg-7">
+      <div class="card shadow mb-4">
+        <!-- Card Header - Dropdown -->
+        <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+          <h6 class="m-0 font-weight-bold text-primary">Purchases Overview</h6>
+          <div class="dropdown no-arrow">
+            <a
+              class="dropdown-toggle"
+              href="#"
+              role="button"
+              id="dropdownMenuLink"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+              <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
+            </a>
+            <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+              <div class="dropdown-header">Dropdown Header:</div>
+              <a class="dropdown-item" href="#">Action</a>
+              <a class="dropdown-item" href="#">Another action</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Something else here</a>
+            </div>
+          </div>
+        </div>
+        <!-- Card Body -->
+        <div class="card-body">
+          <div class="chart-area">
+            <div>
+              <ul class="list-group list-group-flush">
+                <li class="list-group-item">
+                  <div v-for="purchase in purchases" v-bind:key="purchase.id">
+                    <span>{{ purchase.created_at }}</span>
+                    <p>{{ purchase.name }} | {{ purchase.price }}</p>
+                  </div>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <!-- My Lame Ol Code -->
   <div>
     <h2>Create A Budget</h2>
     <form v-on:submit.prevent="budgetCreate()">
