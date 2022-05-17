055186884aa160f8ed73d2b413381fd24af3fb9c
mattjkatz <matthewkatz7@gmail.com>
2022-05-17 11:14:34 -0500

    create finance now works with updated backend, still need to fix frequency

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -326,7 +326,7 @@ export default {
                     <label class="input-group-text" for="inputGroupSelect01">Frequency</label>
                   </div>
                   <select class="custom-select" v-model="newFinance.frequency" id="inputGroupSelect01">
-                    <option v-for="frequency in frequencies" v-bind:key="frequency.id" value="frequency">
+                    <option v-for="frequency in frequencies" v-bind:key="frequency.id" value="{{ frequency }}">
                       {{ frequency }}
                     </option>
                   </select>
