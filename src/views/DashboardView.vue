3e5b21831cbe568f60ac64ce2362b25b6c7611d3
mattjkatz <matthewkatz7@gmail.com>
2022-05-16 22:30:40 -0500

    working main

--- src/views/DashboardView.vue
+++ src/views/DashboardView.vue
@@ -328,7 +328,7 @@ export default {
               <i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
             </a>
             <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-              <div class="dropdown-header">Dropdown Header:</div>
+              <div class="dropdown-header">Purchase Actions:</div>
               <a class="dropdown-item" href="#">Action</a>
               <a class="dropdown-item" href="#">Another action</a>
               <div class="dropdown-divider"></div>
